22f4c74d35b2eff3c052dbf0d4f0f24b41b1df6e
MsKnomr <naomisconrad@outlook.com>
2026-03-23 14:36:00 -0300

    added asteroids class and asteroidfield, spawning them on the screen. When a player collides with an asteroid the game ends.

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
+        for object in drawable:
+            object.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+    
 
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
